59ae1429e005c368ca4f36f47ba2a7c48e6e8cf7
AbdullohMirdadaev <abdullohmirdadaev725@gmail.com>
2024-04-02 13:16:14 +0500

    pls 100 coins

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let son1 = prompt(`String tupedagi malumotni kiriting`)
+let son2 = prompt(`Number tupedagi malumot kiriting`)
+let son3 = prompt(`Boolean tupedagi malumot kiriting`)
+
+console.log(`string tupesi ${son1}`)
+console.log(`number tupesi ${son2}`)
+console.log(`boolean tupesi ${son3}`)
+
+alert("Hamma malumotni tog'ri kiritganingizga ishonchingiz komilmi?")
+confirm("siz kiritgan abdulloh stringa tegishlimi?")
+confirm("siz kiritgan 10 Numberga tegishlimi?")
+confirm("siz kiritgan true booleanga tegishlimi")
---
+++ js/str.js
@@ -0,0 +1,19 @@
+// let str = "Assalomu aleykum bolajonlar!";
+
+// // length -> str ning uzunligi
+// // console.log(str.lenght);
+
+// // let st = str[0]
+// // console.log (st)
+
+// // toUpperCase() -> strdagi hariflarni katta harf qilib beradi
+// // let upper = str.toUpperCase();
+// // console.log(upper);
+// // console.log(str);
+
+// // toLowerCase() -> strdagi hariflarni kichik qilib beradi
+// // container cat is concat
+
+// // replace() -> str ni boshqa str bilan almashtirib beradi
+// // let y = "assamk"
+// // let sa =
